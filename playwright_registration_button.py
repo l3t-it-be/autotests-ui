f1f74d72286f8883fa56212231d16744627b510f
KateBelova <ka7e.online@gmail.com>
2025-03-13 17:34:20 +0300

    5.5 Углубление в Playwright. Шаг 16: Практика по углубленной работе с Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -8,6 +8,11 @@ with sync_playwright() as playwright:
         'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration'
     )
 
+    registration_button = page.get_by_test_id(
+        'registration-page-registration-button'
+    )
+    expect(registration_button).to_be_disabled()
+
     registration_email_input = page.get_by_test_id(
         'registration-form-email-input'
     ).locator('input')
@@ -23,11 +28,4 @@ with sync_playwright() as playwright:
     ).locator('input')
     registration_password_input.fill('password')
 
-    registration_button = page.get_by_test_id(
-        'registration-page-registration-button'
-    )
-    registration_button.click()
-
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_be_visible()
-    expect(dashboard_header).to_have_text('Dashboard')
+    expect(registration_button).to_be_enabled()
