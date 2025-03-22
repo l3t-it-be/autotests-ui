123fda222283ddaf28d3ddaf68f45e89411f2bd3
KateBelova <ka7e.online@gmail.com>
2025-03-22 17:53:59 +0300

    6.3 Маркировки Pytest. Шаг 14: Практика работы с pytest маркировками

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,8 +1,11 @@
 import os
 
+import pytest
 from playwright.sync_api import sync_playwright, expect
 
 
+@pytest.mark.regression
+@pytest.mark.registration
 def test_successful_registration():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
