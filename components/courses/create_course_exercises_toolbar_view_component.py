cf364d3eae4d5747853f5be131a0066b4957b0fb
KateBelova <ka7e.online@gmail.com>
2025-04-10 12:48:02 +0300

    7.6 Практические применение PageFactory в автотестах. Шаг 6: Практикуемся в применении Page Factory

--- components/courses/create_course_exercises_toolbar_view_component.py
+++ components/courses/create_course_exercises_toolbar_view_component.py
@@ -1,24 +1,30 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.button import Button
+from elements.text import Text
 
 
 class CreateCourseExercisesToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.exercises_title = page.get_by_test_id(
-            'create-course-exercises-box-toolbar-title-text'
+        self.exercises_title = Text(
+            page,
+            'Exercises title',
+            'create-course-exercises-box-toolbar-title-text',
         )
-        self.create_exercise_button = page.get_by_test_id(
-            'create-course-exercises-box-toolbar-create-exercise-button'
+        self.create_exercise_button = Button(
+            page,
+            'Create exercise',
+            'create-course-exercises-box-toolbar-create-exercise-button',
         )
 
     def check_visible(self):
-        expect(self.exercises_title).to_be_visible()
-        expect(self.exercises_title).to_have_text('Exercises')
+        self.exercises_title.check_visible()
+        self.exercises_title.check_have_text('Exercises')
 
-        expect(self.create_exercise_button).to_be_visible()
+        self.create_exercise_button.check_visible()
 
     def click_create_exercise_button(self):
         self.create_exercise_button.click()
