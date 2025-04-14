4546711156a8cfdf67fb11829b2eae107efac65f
KateBelova <ka7e.online@gmail.com>
2025-04-14 18:32:12 +0300

    8.2 Интеграция Allure в автотесты. Шаг 16: Практическая работа с Allure аннотациями

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,13 +1,29 @@
+import allure
 import pytest
+from allure_commons.types import Severity
 
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.title('User login with correct email and password')
+    @allure.severity(Severity.BLOCKER)
     def test_successful_authorization(
         self,
         registration_page: RegistrationPage,
@@ -38,18 +54,6 @@ class TestAuthorization:
         dashboard_page.navbar.check_visible('username')
         dashboard_page.sidebar.check_visible()
 
-    def test_navigate_from_authorization_to_registration(
-        self, login_page: LoginPage, registration_page: RegistrationPage
-    ):
-        login_page.visit(
-            'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login'
-        )
-        login_page.click_registration_link()
-
-        registration_page.registration_form.check_visible(
-            email='', username='', password=''
-        )
-
     credentials = {
         'invalid_email_and_password': ('user.name@gmail.com', 'password'),
         'invalid_email_and_empty_password': ('user.name@gmail.com', '  '),
@@ -59,6 +63,9 @@ class TestAuthorization:
     @pytest.mark.parametrize(
         'email, password', credentials.values(), ids=credentials.keys()
     )
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.title('User login with wrong email or password')
+    @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(
         self, login_page: LoginPage, email: str, password: str
     ):
@@ -70,3 +77,18 @@ class TestAuthorization:
         login_page.click_login_button()
 
         login_page.check_visible_wrong_email_or_password_alert()
+
+    @allure.tag(AllureTag.NAVIGATION)
+    @allure.title('Navigation from login page to registration page')
+    @allure.severity(Severity.NORMAL)
+    def test_navigate_from_authorization_to_registration(
+        self, login_page: LoginPage, registration_page: RegistrationPage
+    ):
+        login_page.visit(
+            'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login'
+        )
+        login_page.click_registration_link()
+
+        registration_page.registration_form.check_visible(
+            email='', username='', password=''
+        )
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,25 +1,29 @@
 import os
 
+import allure
 import pytest
+from allure_commons.types import Severity
 
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
-    def test_empty_courses_list(self, courses_list_page: CoursesListPage):
-        courses_list_page.visit(
-            'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses'
-        )
-
-        courses_list_page.navbar.check_visible('username')
-        courses_list_page.sidebar.check_visible()
-
-        courses_list_page.toolbar_view.check_visible()
-        courses_list_page.check_visible_empty_view()
-
+    @allure.title('Create course')
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(
         self,
         create_course_page: CreateCoursePage,
@@ -77,6 +81,8 @@ class TestCourses:
             min_score='10',
         )
 
+    @allure.title('Edit course')
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(
         self,
         create_course_page: CreateCoursePage,
@@ -148,3 +154,16 @@ class TestCourses:
             max_score='250',
             min_score='80',
         )
+
+    @allure.title('Check displaying of empty courses list')
+    @allure.severity(Severity.NORMAL)
+    def test_empty_courses_list(self, courses_list_page: CoursesListPage):
+        courses_list_page.visit(
+            'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses'
+        )
+
+        courses_list_page.navbar.check_visible('username')
+        courses_list_page.sidebar.check_visible()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.check_visible_empty_view()
